1daf456bb42ab68f10b3baa3866390ca4c16c8f6
Mustafa Ateş <mstfaatesss@gmail.com>
2021-04-07 21:21:45 +0300

    pipes, cartItem added

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,7 +1,9 @@
 import { HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -12,11 +14,13 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductComponent implements OnInit {
   products: Product[] = [];
   dataLoaded = false;
-  filterText="";
+  filterText = '';
 
   constructor(
     private productService: ProductService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private toastrService: ToastrService,
+    private cartService: CartService,
   ) {}
 
   ngOnInit(): void {
@@ -43,5 +47,14 @@ export class ProductComponent implements OnInit {
         this.products = response.data;
         this.dataLoaded = true;
       });
+  }
+
+  addToCart(product: Product) {
+    if (product.productId === 1) {
+      this.toastrService.error('Hata', 'Bu ürün sepete eklenemez');
+    } else {
+      this.toastrService.success('Sepete Eklendi', product.productName);
+      this.cartService.addToCart(product);
+    }
   }
 }
